fda5ab74667eb83689c8882284e22c22b8be4f32
Sean Hellum <seanhellum45@gmail.com>
2020-02-18 18:54:45 +0000

    add stuff to screen

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-#![no_std] // Do not link std lib
-#![no_main] // Disable Rust entry points
+#![no_std]
+#![no_main]
+
 use core::panic::PanicInfo;
 
-#[no_mangle] // Do not mangle name
+static HELLO: &[u8] = b"Hello World From RustOS";
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
-// Call this function on panic
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
