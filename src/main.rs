db40e8a6f75cf1e5c039618931b223db7c6612f3
Sean Hellum <seanhellum45@gmail.com>
2020-02-18 17:37:20 +0000

    Finish freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-#![no_std]
-#![no_main]
+#![no_std] // Do not link std lib
+#![no_main] // Disable Rust entry points
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle] // Do not mangle name
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+// Call this function on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
