3dde38eab591d20afe172ef52e47af954776e0fd
Sean Hellum <seanhellum45@gmail.com>
2020-02-19 00:29:59 +0000

    Change text coloring of VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xa;
         }
     }
 
