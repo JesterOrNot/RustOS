d12074c891858cfcd7d8a516d7296db0b7fd146e
Sean Hellum <seanhellum45@gmail.com>
2020-02-19 21:37:54 +0000

    [Abstraction] VGA Text Buffer interface

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@
 #![no_main] // Disable Rust entry points
 #![allow(non_snake_case)]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World From RustOS!";
+static MESSAGE_TO_DISPLAY: &[u8] = b"Hello World From RustOS!";
 
 #[no_mangle] // Do not mangle name
 pub extern "C" fn _start() -> ! {
+    // This function returns the never type which means it will never return
     // this method is the entry point, since the linker looks for a method
     // named `_start` by default
 
     let vga_text_buffer = 0xb8000 as *mut u8; // This is the memory adress of the vga_text_buffer
 
-    for (i, &byte) in HELLO.iter().enumerate() {
+    for (i, &byte) in MESSAGE_TO_DISPLAY.iter().enumerate() {
         unsafe {
             *vga_text_buffer.offset(i as isize * 2) = byte;
             *vga_text_buffer.offset(i as isize * 2 + 1) = 0xa;
@@ -23,7 +26,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-/// This method is called on panic.
+// This method is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
---
+++ src/vga_buffer.rs
@@ -0,0 +1,85 @@
+/*
+This enum won't be used in this file this is just a library to encapsulate vga_buffer related things
+*/
+#[allow(dead_code)]
+/* Makes printable, adds copy semantics
+   and adds the ability to compare them
+*/
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    // To represent the color codes
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ColorCode(u8);
+
+impl ColorCode {
+    pub fn new(foreground_color: Color, background_color: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8, // A vga char consists of an ansi code and a color code
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,=
+    buffer: &'static mut Buffer, // Static means is valid for whole program duration
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) { /* WIP */ }
+
+}
